027b72187513d0fab03353102f127c84482503d3
Ethan Krich <ektechworld@gmail.com>
2026-05-22 18:02:59 -0700

    fix: position toast on bottom-left

--- apps/editor/src/components/ui/sonner.tsx
+++ apps/editor/src/components/ui/sonner.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner, type ToasterProps } from "sonner";
 const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
+      position="bottom-left"
       className="toaster group"
       style={
         {
